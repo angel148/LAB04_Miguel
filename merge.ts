d8cc01fef174445e2eaa261825db814ca8695e97
angel <aburgoapaco@gmail.com>
2024-03-24 22:19:35 -0400

    feature(common): added merge

---
+++ merge.ts
@@ -0,0 +1,3 @@
+//Una vez creada la ultima rama entre a  gihub e  hice
+// un PR despues creada el PR hice MERGE y elimine la rama la ultima que cree y al  momento 
+// de hacer merge se unio con la rama main
